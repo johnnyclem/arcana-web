60fdcac9cfd943740cabe8f38e5b14304c9011b4
James McConnell <interneth3ro@gmail.com>
2014-02-06 11:57:48 -0600

    Updated UI
    Started importing new UI

--- Arcana/Arcana.Web/Controllers/HomeController.cs
+++ Arcana/Arcana.Web/Controllers/HomeController.cs
@@ -11,21 +11,12 @@ namespace Arcana.Web.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult QuickGame()
         {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Title = "Quick Game";
             return View();
         }
     }
